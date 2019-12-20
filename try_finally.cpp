0104fec1c19478ff7aff8d0c65fdffea7433617c
Hyunjik Bae <imays76@gmail.com>
2019-12-20 12:34:23 +0900

    ...

--- try_finally.cpp
+++ try_finally.cpp
@@ -4,43 +4,43 @@ int c = 0; // 0: default / 1: something doing / 2: something doing more / ...
 
 void foo()
 {
-	try
-	{
-		a = 1;
-		b = 2;
-	}
-	finally
-	{
-		a = 0;
-		b = 0;
-	}
+    try
+    {
+        a = 1;
+        b = 2;
+    }
+    finally
+    {
+        a = 0;
+        b = 0;
+    }
 }
 
 void foo1()
 {
-	try
-	{
-		a = 3;
-		c = 5;
-	}
-	finally
-	{
-		a = 0;
-		c = 0; // this time, we rollback not b but c intentionally.
-	}
+    try
+    {
+        a = 3;
+        c = 5;
+    }
+    finally
+    {
+        a = 0;
+        c = 0; // this time, we rollback not b but c intentionally.
+    }
 }
 
 void foo2()
 {
-	assert(a == 1);	// by contract 
-	try
-	{
-		a = 3;	
-		c = 4;
-	}
-	finally
-	{
-		a = 1;		// notice that a must be rolled back to 1, not 0! 
-		c = 0;
-	}
+    assert(a == 1);	// by contract 
+    try
+    {
+        a = 3;	
+        c = 4;
+    }
+    finally
+    {
+        a = 1;		// notice that a must be rolled back to 1, not 0! 
+        c = 0;
+    }
 }
